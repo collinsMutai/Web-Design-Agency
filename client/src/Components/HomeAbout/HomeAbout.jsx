399b107b230af0df1ce8f430340ace88fc8f9eba
Collins Mutai <collinsfrontend@gmail.com>
2024-04-01 21:00:59 +0300

    homeabout section

---
+++ client/src/Components/HomeAbout/HomeAbout.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./HomeAbout.css";
+import home_workplace_image from "../../Images/workplace2.jpeg";
+
+const HomeAbout = () => {
+  return (
+    <div className="container">
+      <div className="left">
+        <h3>Digital consulting</h3>
+        <h2>Tailored digital solutions for your business</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+          laborum similique enim et tenetur ducimus rem magni. Distinctio,
+          dolorem eligendi.
+        </p>
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 arrow-right"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ducimus!
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 arrow-right"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ducimus!
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 arrow-right"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ducimus!
+          </li>
+        </ul>
+        <button>Discover more</button>
+      </div>
+      <div className="right">
+        <img src={home_workplace_image} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomeAbout;

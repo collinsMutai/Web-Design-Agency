78da8ac02f772cb423c60aa2e6d21fc9cd13d248
Collins Mutai <collinsfrontend@gmail.com>
2024-04-08 22:35:29 +0300

    media query

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <>
       <Hero />
-      {/* <HomeAbout /> */}
+      <HomeAbout />
       {/* <FeaturedServices />
       <FeaturedProjects />
       <HappyClients />

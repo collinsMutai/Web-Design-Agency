bdffe24087d85504569241813d2960155a8b762f
Collins Mutai <collinsfrontend@gmail.com>
2024-04-10 21:52:23 +0300

    featured projects media query

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -12,8 +12,8 @@ const HomePage = () => {
       <Hero />
       <HomeAbout />
       <FeaturedServices />
-      {/* <FeaturedProjects />
-      <HappyClients />
+      <FeaturedProjects />
+      {/* <HappyClients />
       <Cta /> */}
     </>
   );

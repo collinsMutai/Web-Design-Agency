0341c57072e4e6eceafa4cf12ed8df669c7f822a
Collins Mutai <collinsfrontend@gmail.com>
2025-09-12 20:19:58 +0300

    ui

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(
 
 // ✅ Headers middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://collinsmutai.com");
+  // res.header("Access-Control-Allow-Origin", "https://collinsmutai.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");

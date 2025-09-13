19458f5ba924a66efabde2b7d0b45dc772f3a332
Collins Mutai <collinsfrontend@gmail.com>
2025-09-13 08:24:45 +0300

    ui

--- backend/app.js
+++ backend/app.js
@@ -22,14 +22,13 @@ mongoose
 // ✅ CORS setup
 app.use(
   cors({
-    origin: "https://collinsmutai.com",
+    origin: process.env.FRONTEND_URLS.split(','),
     credentials: true,
   })
 );
 
 // ✅ Headers middleware
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "https://collinsmutai.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
